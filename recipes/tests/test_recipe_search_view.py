e9147d0573318474dca3738d3d3b3effbfe06619
IsraelRAlencar <israelAlencar@gecontrolsistemas.com.br>
2023-10-13 14:48:40 -0400

    finalizado modulo testes

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -24,3 +24,22 @@ class RecipeSearchViewTest(RecipeTestBase):
             'Search results for &quot;&lt;teste&gt;&quot;',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title1 = 'This is recipe one'
+        title2 = 'This is recipe two'
+
+        recipe1 = self.make_recipe(title=title1, slug='recipe-one', author_data={'username': 'user1'})  # noqa e501
+        recipe2 = self.make_recipe(title=title2, slug='recipe-two', author_data={'username': 'user2'})  # noqa e501
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?q={title1}')
+        response2 = self.client.get(f'{search_url}?q={title2}')
+        response_both = self.client.get(f'{search_url}?q=this')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
